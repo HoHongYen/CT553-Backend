ab10a96565be0f650d3331f450bf8b04eae08c9c
Ho Hong Yen <yenvt9a1@gmail.com>
2024-09-22 23:21:04 +0700

    feat: Handle upload temp data (uploadOrderStatuses)

--- src/services/uploadTempData/basic.js
+++ src/services/uploadTempData/basic.js
@@ -1,11 +1,14 @@
 const prisma = require("../../config/prismaClient");
+const { AWAITING_CONFIRM, AWAITING_FULFILLMENT, DELIVERING, DELIVERED, CANCELED, RETURNED } = require("../../constant/orderStatus");
+const { PENDING, SUCCESS, FAILED } = require("../../constant/paymentStatus");
 
 async function uploadRoles() {
+    // delete all roles
     await prisma.role.createMany({
         data: [
             { role: "ADMIN" },
             { role: "EMPLOYEE" },
-            { role: "USER" }
+            { role: "USER" },
         ]
     });
 }
@@ -22,11 +25,24 @@ async function uploadPaymentMethods() {
 async function uploadPaymentStatuses() {
     await prisma.paymentStatus.createMany({
         data: [
-            { name: "PENDING" },
-            { name: "SUCCESS" },
-            { name: "FAILED" },
+            { name: PENDING },
+            { name: SUCCESS },
+            { name: FAILED },
         ]
     });
 }
 
-module.exports = { uploadRoles, uploadPaymentMethods, uploadPaymentStatuses };
+async function uploadOrderStatuses() {
+    await prisma.orderStatus.createMany({
+        data: [
+            { name: AWAITING_CONFIRM },
+            { name: AWAITING_FULFILLMENT },
+            { name: DELIVERING },
+            { name: DELIVERED },
+            { name: CANCELED },
+            { name: RETURNED },
+        ]
+    });
+}
+
+module.exports = { uploadRoles, uploadPaymentMethods, uploadPaymentStatuses, uploadOrderStatuses };
